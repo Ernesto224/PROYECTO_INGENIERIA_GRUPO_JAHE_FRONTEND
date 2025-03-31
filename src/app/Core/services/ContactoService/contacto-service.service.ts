3e073d88d332a6d3eb729efd80178df86981f24d
JESNER1680 <129021529+JESNER1680@users.noreply.github.com>
2025-03-31 01:48:44 -0600

    Contáctanos realizado

---
+++ src/app/Core/services/ContactoService/contacto-service.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ContactoDTO } from '../../models/ContactoDTO';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactoServiceService {
+  constructor(private http: HttpClient) {}
+  contactanosUrl = 'https://localhost:7169/api/Contacto';
+
+  obtenerDatosContactanos(): Observable<ContactoDTO> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      accept: 'application/json',
+    });
+    return this.http.get<ContactoDTO>(`${this.contactanosUrl}`, {
+      headers,
+    });
+  }
+}
